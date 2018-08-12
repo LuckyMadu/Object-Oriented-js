9a13bf18456eb8b66c7d0fd57a4daa494eec92bc
lahiru <lahiruamarathunga526@gmail.com>
2018-08-12 13:33:58 +0530

    class method modified

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ class User {
     this.email = email;
     this.name = name;
   }
+
+  login() {
+    console.log(this.email, "just logged in");
+  }
+  logout() {
+    console.log(this.email, "just logged out");
+  }
 }
 
 var userOne = new User("lahiru@frontdaddy.com", "Lahiru");
 var userTwo = new User("madu@frontdaddy.com", "Madu");
+
+userOne.login();
+userTwo.logout();
