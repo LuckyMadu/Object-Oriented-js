f4769574f3281b1b7c9861a8f06dc2c2ca639fd5
lahiru <lahiruamarathunga526@gmail.com>
2018-08-12 16:39:27 +0530

    constructors

--- index.js
+++ index.js
@@ -1,40 +1,14 @@
-class User {
-  constructor(email, name) {
-    this.email = email;
-    this.name = name;
-    this.score = 0;
-  }
-
-  login() {
-    console.log(this.email, "just logged in");
-    return this;
-  }
-  logout() {
-    console.log(this.email, "just logged out");
-    return this;
-  }
-  updateScore() {
-    this.score++;
-    console.log(this.email, "score is now ", this.score);
-    return this;
-  }
-}
-
-class Admin extends User {
-  deleteUser(user) {
-    users = users.filter(u => {
-      //ACCESS FOR INDIVIDUAL ITEMS
-      return u.email != user.email;
-    });
-  }
+function User(email, name) {
+  this.email = email;
+  this.name = name;
+  this.online = false;
+  this.login = function() {
+    console.log(this.email, "has logged in");
+  };
 }
 
 var userOne = new User("lahiru@frontdaddy.com", "Lahiru");
 var userTwo = new User("madu@frontdaddy.com", "Madu");
-var admin = new Admin("shaan@frontdaddy.com", "shaan");
 
-var users = [userOne, userTwo, admin];
-
-admin.deleteUser(userTwo);
-
-console.log(users);
+console.log(userOne);
+userTwo.login();
