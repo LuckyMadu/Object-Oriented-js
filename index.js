e0b75ecae2a36e9b5f1cb26ea32246fdca0b1b53
lahiru <lahiruamarathunga526@gmail.com>
2018-08-12 12:07:42 +0530

    javascript classes

---
+++ index.js
@@ -0,0 +1,14 @@
+class User {
+  constructor(email, name) {
+    this.email = email;
+    this.name = name;
+  }
+}
+
+var userOne = new User("lahiru@frontdaddy.com", "Lahiru");
+var userTwo = new User("madu@frontdaddy.com", "Madu");
+
+console.log(userOne);
+console.log(userTwo);
+// the 'new'
+//create a new empty object
