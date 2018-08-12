48bd645c6f4223f6ba4be99616f2116eeb84bb7a
lahiru <lahiruamarathunga526@gmail.com>
2018-08-12 18:36:08 +0530

    prototypes-inheritance

--- index.js
+++ index.js
@@ -13,8 +13,23 @@ User.prototype.logout = function() {
   console.log(this.email, "has logged out");
 };
 
+function Admin(...args) {
+  //grap email and name as an array
+  //console.log(args);
+  this.role = "super admin";
+  User.apply(this, args);
+}
+
+Admin.prototype = Object.create(User.prototype);
+Admin.prototype.deleteUser = function(u) {
+  users = users.filter(user => {
+    return user.email != u.email;
+  });
+};
+
 var userOne = new User("lahiru@frontdaddy.com", "Lahiru");
 var userTwo = new User("madu@frontdaddy.com", "Madu");
+var admin = new Admin("shan@frontdaddy.com", "shan");
 
-console.log(userOne);
-userTwo.login();
+var users = [userOne, userTwo, admin];
+console.log(admin);
